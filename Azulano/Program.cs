6f2e981ad16faa8bc558e61340bcc654df0c1c30
Silas Henrique <silas.henrique21@outlook.com>
2024-06-05 22:40:58 -0300

    Alteração banco de dados

--- Azulano/Program.cs
+++ Azulano/Program.cs
@@ -1,7 +1,15 @@
+using Azulano.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Adicionar a configuração do DbContext
+builder.Services.AddDbContext<MapeamentoAnimaisMarinhosDBContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,7 +17,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
